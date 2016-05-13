032cbc90b33f7bf3a17e5cc1ed6498b55479dc70
pedrofelipefroes <pedrofelipefroes@gmail.com>
2016-05-12 21:39:53 -0300

    Exercise 2 completed

--- server/app.js
+++ server/app.js
@@ -7,22 +7,21 @@ var http = require('http');
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
 // dica: 3-4 linhas de código (você deve usar o módulo de filesystem (fs))
 var db = {};
-
 db.jogadores = JSON.parse(fs.readFileSync('server/data/jogadores.json', 'utf8'));
 db.jogosPorJogador = JSON.parse(fs.readFileSync('server/data/jogosPorJogador.json', 'utf8'));
 
 // configurar qual templating engine usar. Sugestão: hbs (handlebars)
 app.set('view engine', 'hbs');
 
-
 // EXERCÍCIO 2
 // definir rota para página inicial --> renderizar a view index, usando os
 // dados do banco de dados "data/jogadores.json" com a lista de jogadores
 // dica: o handler desta função é bem simples - basta passar para o template
 //       os dados do arquivo data/jogadores.json
+
 app.set('views', 'server/views');
 app.get('/', function (req, res) {
-    res.render('index');
+    res.render('index', {players: db.jogadores.players}); // esse render precisa exportar os jogadores
 });
 
 // EXERCÍCIO 3
