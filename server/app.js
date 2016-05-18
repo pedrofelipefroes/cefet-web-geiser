36732c2efab8a8d7d58064b36c3a61306767ee32
pedrofelipefroes <pedrofelipefroes@gmail.com>
2016-05-17 21:42:37 -0300

    Final commit

--- server/app.js
+++ server/app.js
@@ -3,6 +3,7 @@ var express = require('express'),
 
 var fs = require('fs');
 var http = require('http');
+var _ = require('underscore');
 // carregar "banco de dados" (data/jogadores.json e data/jogosPorJogador.json)
 // você pode colocar o conteúdo dos arquivos json no objeto "db" logo abaixo
 // dica: 3-4 linhas de código (você deve usar o módulo de filesystem (fs))
@@ -20,9 +21,37 @@ app.set('view engine', 'hbs');
 //       os dados do arquivo data/jogadores.json
 
 app.set('views', 'server/views');
+
 app.get('/', function (req, res) {
+    // index
     res.render('index', { player: db.jogadores.players }); // esse render precisa exportar os jogadores
 });
+
+app.get('/jogador/:id', function (req, res) {
+    // jogador
+    var playerID = req.params.id;
+    
+    var playerProfile = _.find(db.jogadores.players, function(player) {
+        return player.steamid === playerID;
+    });
+    
+    var playerGames = db.jogosPorJogador[playerID];
+    
+    playerGames.games = _.sortBy(playerGames.games, function(game) {
+        return (game.playtime_forever)*(-1);
+    });
+    
+    playerGames.games = _.map(playerGames.games, function(game) {
+        game.playtime_forever = Math.round(game.playtime_forever/60);
+        return game;
+    });
+    
+    playerGames.games = _.first(playerGames.games, 5);
+    
+    playerGames.unplayed = _.where(playerGames.games, { playtime_forever: 0 }).length;
+    
+    res.render('jogador', { player: playerProfile, games: playerGames, favorite: playerGames.games[0] });
+})
 
 // EXERCÍCIO 3
 // definir rota para página de detalhes de um jogador --> renderizar a view
